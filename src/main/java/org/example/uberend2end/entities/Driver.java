a0421ad1d3e0ac5bbab35c6498e76b8caba54e43
anni2909 <dhanawadeaniket72@gmail.com>
2026-02-05 22:36:32 -0500

    added Driver model

---
+++ src/main/java/org/example/uberend2end/entities/Driver.java
@@ -0,0 +1,23 @@
+package org.example.uberend2end.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "drivers")
+@Getter
+@Setter
+public class Driver extends BaseEntity {
+
+    @DBRef
+    private User user;
+
+    private Double rating;
+
+    private Boolean available;
+
+    private String vehicleId;
+
+
+}
